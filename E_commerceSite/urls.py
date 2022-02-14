fab59027543b2cc02fd03895db281321a1427471
rupeshthapa123 <rupesh.thapa2050@gmail.com>
2022-02-14 14:27:14 +0545

    first commit

---
+++ E_commerceSite/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+from django.conf.urls import url
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('djoser.urls')),
+    path('api/v1/', include('djoser.urls.authtoken')),
+    path('api/v1/', include('product.urls')),
+    path('api/v1/', include('order.urls')),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
